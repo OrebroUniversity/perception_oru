2a64ef7e159b326e28a0650f1c027c0d2ecfa5ec
Martin Magnusson <martin.magnusson@oru.se>
2014-10-14 15:44:32 +0200

    Changed %lu to %u to avoid warnings

--- ndt_visualisation/include/ndt_visualisation/ndt_viz.h
+++ ndt_visualisation/include/ndt_visualisation/ndt_viz.h
@@ -93,7 +93,7 @@ class NDTViz {
 	    
 	    unsigned int accepted_ndts=1;
 	    double x = 0,y=0,s=0;
-	    fprintf(stderr,"-NDT:%lu-",global_ndts.size());
+	    fprintf(stderr,"-NDT:%u-",global_ndts.size());
 	    for(unsigned int i=0;i<global_ndts.size();i++){
 		Eigen::Vector3d m = global_ndts[i]->getMean();
 		if(!global_ndts[i]->hasGaussian_) continue;
@@ -132,7 +132,7 @@ class NDTViz {
 	    if(win3D == NULL) return;
 	    std::vector<lslgeneric::NDTCell*> global_ndts;
 	    global_ndts = map->getAllCells();
-	    fprintf(stderr," NUM NDT: %lu ", global_ndts.size());
+	    fprintf(stderr," NUM NDT: %u ", global_ndts.size());
 
 	    win3D->clearScene();
 	    gl_ellipsoids.clear();
@@ -174,7 +174,7 @@ class NDTViz {
 	    if(win3D == NULL) return;
 	    std::vector<lslgeneric::NDTCell*> global_ndts;
 	    global_ndts = map->getAllCells();
-	    fprintf(stderr," NUM NDT: %lu ", global_ndts.size());
+	    fprintf(stderr," NUM NDT: %u ", global_ndts.size());
 
 	    Eigen::Vector3d cFlat, cInclined, cRough, cVert, cUnknown, color;
 	    cFlat<<0,0.9,0;
@@ -238,7 +238,7 @@ class NDTViz {
 	  if(win3D == NULL) return;
 	    std::vector<lslgeneric::NDTCell*> global_ndts;
 	    global_ndts = map->getAllCells();
-	    fprintf(stderr," NUM NDT: %lu ", global_ndts.size());
+	    fprintf(stderr," NUM NDT: %u ", global_ndts.size());
 
 	    win3D->clearScene();
 	    gl_ellipsoids.clear();

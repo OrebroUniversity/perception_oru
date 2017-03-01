b9a509bf1f134424b07545d04e005e078f680258
Henrik Andreasson <henrik.andreasson@oru.se>
2017-03-01 15:19:58 +0100

    Added evaluation scrips.

---
+++ ndt_registration/test/scripts/get_stat_filter_eval.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+FILES=./*Ts.gt
+# d2d_est_end='Ts.d2d'
+# d2d_sc_est_end='Ts.d2d_sc'
+# filter_est_end='Ts.filter'
+est_end='.Ts.filter'
+png_end='.png'
+txt_end='.txt'
+rot='rot'
+pos='pos'
+rpe='rpe'
+for gt in $FILES
+do
+  # Remove the ending _gt.txt
+    base_name=${gt%.Ts.gt}
+    echo "Processing $base_name files..."
+
+    est=$base_name$est_end
+    echo $est
+
+    png=$base_name$est_end$png_end
+    txt=$base_name$est_end$txt_end
+
+    # Processing 
+    echo "rosrun ndt_feature_reg evaluate_ate.py $est $gt --plot $png --verbose > $txt"
+          rosrun ndt_feature_reg evaluate_ate.py $est $gt --plot $png --verbose > $txt
+    echo "rosrun ndt_feature_reg evaluate_rpe.py $est $gt --plot $est$pos$png_end --plot_rot $est$rot$png_end --fixed_delta --verbose > $est$rpe$txt_end"
+          rosrun ndt_feature_reg evaluate_rpe.py $est $gt --plot $est$pos$png_end --plot_rot $est$rot$png_end --fixed_delta --verbose > $est$rpe$txt_end
+
+done

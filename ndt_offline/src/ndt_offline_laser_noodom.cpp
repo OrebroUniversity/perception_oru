b69425244483beaba4e7ca633a8425fc27011504
Malcolm Mielle <malcolm.mielle@protonmail.com>
2017-10-05 19:04:17 +0200

    fixed errors in ndt_offline

--- ndt_offline/src/ndt_offline_laser_noodom.cpp
+++ ndt_offline/src/ndt_offline_laser_noodom.cpp
@@ -19,8 +19,8 @@
 
 #include <ndt_map/NDTMapMsg.h>
 #include <ndt_map/ndt_conversions.h>
-#include <ndt_fuser_ros_wrappers/ros_fuser_init.hpp>
-#include <ndt_fuser_ros_wrappers/ndt_fuser_logger.hpp>
+#include <ndt_fuser/ndt_fuser_ros_wrappers/ros_fuser_init.hpp>
+#include <ndt_fuser/ndt_fuser_ros_wrappers/ndt_fuser_logger.hpp>
 
 
 #include "ndt_offline/LaserBagReader.hpp"
